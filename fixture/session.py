f07814dcf00bfddcffef837192f57398eb3513c1
Sandmir <sandmir2010@gmail.com>
2017-10-16 17:00:12 -0700

    add tests: login/out - 1.valid credentials,                    2.valid login + invalid psw

--- fixture/session.py
+++ fixture/session.py
@@ -1,4 +1,5 @@
 import time
+import allure
 
 class SessionHelper:
     main_menu_xp = '//div[@class="mdi mdi-menu"]'
@@ -11,18 +12,28 @@ class SessionHelper:
     greeting_xp = '//h1[1]'
     return_button_xp = '//div[text()="Return to homepage"]'
 
+    invalid_cred_xp = "//*[@class='context-info']"
+    forgot_psw_xp = "//*[text()='Forgot Password?']"
+    forgot_psw_field_xp = "//input[@class='input__input-control']"
+    input_forget_psw = "//div[text()='Password']/../input"
+    reset_psw = "//*[text()='Reset Password']"
+    send_new_psw_message_xp = "//*[text()='A new password has been sent to your EMail address.']"
+
+    reset_psw_error = "//*[text()='Warning: The E-Mail Address was not found in our records, please try again!']"
+
     def __init__(self,app):
         self.app = app
 
     def login(self,username, psw):
-        driver = self.app.driver
-        self.app.open_home_page()
-        driver.find_element_by_xpath(self.main_menu_xp).click()
-        time.sleep(2)
-        driver.find_element_by_xpath(self.main_login_button_xp).click()
-        driver.find_element_by_xpath(self.email_input_xp).send_keys(username)
-        driver.find_element_by_xpath(self.password_input_xp).send_keys(psw)
-        driver.find_element_by_xpath(self.login_button_xp).click()
+        with allure.step('Login step'):
+            driver = self.app.driver
+            self.app.open_home_page()
+            driver.find_element_by_xpath(self.main_menu_xp).click()
+            time.sleep(2)
+            driver.find_element_by_xpath(self.main_login_button_xp).click()
+            driver.find_element_by_xpath(self.email_input_xp).send_keys(username)
+            driver.find_element_by_xpath(self.password_input_xp).send_keys(psw)
+            driver.find_element_by_xpath(self.login_button_xp).click()
 
 
     def logout(self):
--- test/login.py
+++ test/login.py
@@ -1,5 +1,7 @@
 import pytest
+import allure
 from fixture.application import Application
+from time import sleep
 
 
 @pytest.fixture
@@ -11,6 +13,18 @@ def app(request):
 
 def test_login_valid_credentials(app):
     app.session.login('testmobile.marina@gmail.com', 'c980159c4c')
+    sleep(2)
+    with allure.step('Verification - valid credentials'):
+        greeting = app.driver.find_element_by_xpath(app.session.greeting_xp).text
+        assert greeting.split(",")[0] == "Welcome back"
 
+
+def test_login_invalid_psw(app):
+    app.session.login('testmobile.marina@gmail.com', '12345')
+    sleep(2)
+    with allure.step('Get error message.'):
+        greeting = app.driver.find_element_by_xpath(app.session.invalid_cred_xp).text
+    with allure.step('Verify error message: Warning: No match for E-Mail Address and/or Password.'):
+        assert greeting.split(",")[0] == "Warning: No match for E-Mail Address and/or Password."
 
 
